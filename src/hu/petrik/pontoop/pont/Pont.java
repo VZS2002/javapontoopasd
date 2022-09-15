6829662140fbf69c8e977f1575d8a12f0962bc4b
PetrikDiak <petrikdiak@gmail.com>
2022-09-15 09:54:31 +0200

    asd

---
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -0,0 +1,34 @@
+package hu.petrik.pontoop.pont;
+public class Pont {
+    private int x;
+    private int y;
+    public Pont(){
+        x = 0;
+        y = 0;
+    }
+
+    public Pont(int x, int y){
+
+        this.x=x;
+        this.y=y;
+    }
+
+    public Pont(int n) {
+        this.x = koordinataGeneralasa(n);
+        this.y = koordinataGeneralasa(n);
+    }
+
+    private int koordinataGeneralasa(int n) {
+        return (int)(Math.random() * ((2*n) + 1)) - n;
+    }
+
+    public double getOrigotolMertTavolsag() {
+        return Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2));
+    }
+    @Override
+    public String toString() {
+       return String.format("(%d, %d)", this.x, this.y);
+
+    }
+}
+
